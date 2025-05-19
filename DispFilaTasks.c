113b01b459546e502326280400578e381182687a
Maic <maic1405@gmail.com>
2025-05-19 19:56:07 -0300

    FEAT: inclusão de tarefas para o buzzer e a matriz de leds

--- DispFilaTasks.c
+++ DispFilaTasks.c
@@ -2,6 +2,8 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 #include "hardware/i2c.h"
+#include "hardware/clocks.h"
+#include "pio_matrix.pio.h"
 #include "lib/ssd1306.h"
 #include "lib/font.h"
 #include "hardware/pwm.h"
@@ -26,6 +28,61 @@ typedef struct {
 } joystick_data_t;
 
 QueueHandle_t xQueueJoystickData;
+bool estadoCriticoVolumeAgua = false, estadoCriticoChuva = false;
+
+//------------- Inicio das estruturas e funções usadas pra matriz de leds -------------//
+typedef struct {
+  double r;
+  double g;
+  double b;
+} Pixel;
+
+Pixel desenho[25] = { 
+    {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0},
+    {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0},
+    {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0},
+    {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0},
+    {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}, {0.01, 0.0, 0.0}
+};
+
+//rotina para definição da intensidade de cores do led
+uint32_t matrix_rgb(double b, double r, double g) {
+  unsigned char R, G, B;
+  R = r * 255;
+  G = g * 255;
+  B = b * 255;
+  return (G << 24) | (R << 16) | (B << 8);
+}
+
+//rotina para acionar a matrix de leds - ws2812b
+void desenho_pio(PIO pio, uint sm) {
+    uint32_t valor_led;
+
+    for (int16_t i = 0; i < 25; i++) {
+        valor_led = matrix_rgb(
+        desenho[24-i].b,  // azul
+        desenho[24-i].r,  // vermelho
+        desenho[24-i].g   // verde
+        );
+        pio_sm_put_blocking(pio, sm, estadoCriticoChuva||estadoCriticoVolumeAgua?valor_led:0);
+    }
+}
+//------------- Fim das estruturas e funções usadas usadas pra matriz de leds -------------//
+
+// Tarefa de controle da matriz de LEDs
+void vLedMatrixTask() {
+    //configurações da PIO
+    PIO pio = pio0;
+    set_sys_clock_khz(128000, false);
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, 7);
+
+    while(1) {
+        desenho_pio(pio, sm); // Chama a função para desenhar na matriz
+        vTaskDelay(1); // Yield para outras tarefas
+    }
+}
 
 void vJoystickTask(void *params) {
     adc_gpio_init(ADC_JOYSTICK_Y);
@@ -63,62 +120,86 @@ void vDisplayTask(void *params) {
     bool cor = true;
     while (true) {
         if (xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE) {
-            // uint8_t x = (joydata.x_pos * (128 - tam_quad)) / 4095;
-            // uint8_t y = (joydata.y_pos * (64 - tam_quad)) / 4095;
-            float xi = joydata.x_pos*100/4095.0;
-            float yi = joydata.y_pos*100/4095.0;
-            sprintf(str, "X: %.0f Y: %.0f", xi, yi);
-            // y = (64 - tam_quad) - y;                                 // Inverte o eixo Y
-            ssd1306_fill(&ssd, !cor);                                // Limpa a tela
+            float x = joydata.x_pos*100/4095.0;
+            float y = joydata.y_pos*100/4095.0;
+
+            ssd1306_fill(&ssd, !cor); // Limpa a tela
+            sprintf(str, "Agua: %.0f", x);
             ssd1306_draw_string(&ssd, str, 0, 0);
-            // ssd1306_rect(&ssd, y, x, tam_quad, tam_quad, cor, !cor); // Quadrado 5x5
+            sprintf(str, "Chuvas: %.0f", y);
+            ssd1306_draw_string(&ssd, str, 0, 12);
+
+            if(x>70) estadoCriticoVolumeAgua = true; else estadoCriticoVolumeAgua = false;
+            if(y>80) estadoCriticoChuva = true; else estadoCriticoChuva = false;
+            if(estadoCriticoVolumeAgua || estadoCriticoChuva) ssd1306_draw_string(&ssd, "ALERTA!", 40, 40);
+                    
             ssd1306_send_data(&ssd);
         }
     }
 }
 
-void vLedGreenTask(void *params) {
-    gpio_set_function(LED_GREEN, GPIO_FUNC_PWM);   // Configura GPIO como PWM
-    uint slice = pwm_gpio_to_slice_num(LED_GREEN); // Obtém o slice de PWM
-    pwm_set_wrap(slice, 100);                     // Define resolução (0–100)
-    pwm_set_chan_level(slice, PWM_CHAN_B, 0);     // Duty inicial
-    pwm_set_enabled(slice, true);                 // Ativa PWM
+void vLedTask(void *params) {
+    gpio_init(13); // Inicializa o LED
+    gpio_set_dir(13, GPIO_OUT); // Configura como saída
 
     joystick_data_t joydata;
     while (true) {
         if (xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE) {
-            // Brilho proporcional ao desvio do centro
-            int16_t desvio_centro = (int16_t)joydata.x_pos - 2000;
-            if (desvio_centro < 0)
-                desvio_centro = -desvio_centro;
-            uint16_t pwm_value = (desvio_centro * 100) / 2048;
-            pwm_set_chan_level(slice, PWM_CHAN_B, pwm_value);
+            // Desliga ou liga o LED vermelho com base nos estados críticos
+            if (estadoCriticoVolumeAgua || estadoCriticoChuva) gpio_put(13, 1); else gpio_put(13, 0);
         }
         vTaskDelay(pdMS_TO_TICKS(50)); // Atualiza a cada 50ms
     }
 }
 
-void vLedBlueTask(void *params) {
-    gpio_set_function(LED_BLUE, GPIO_FUNC_PWM);   // Configura GPIO como PWM
-    uint slice = pwm_gpio_to_slice_num(LED_BLUE); // Obtém o slice de PWM
-    pwm_set_wrap(slice, 100);                     // Define resolução (0–100)
-    pwm_set_chan_level(slice, PWM_CHAN_A, 0);     // Duty inicial
-    pwm_set_enabled(slice, true);                 // Ativa PWM
+void vBuzzerTask(void *params) {
+    static int freq = 0;
+    bool subindo = true;
 
-    joystick_data_t joydata;
-    while (true) {
-        if (xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE) {
-            // Brilho proporcional ao desvio do centro
-            int16_t desvio_centro = (int16_t)joydata.y_pos - 2048;
-            if (desvio_centro < 0)
-                desvio_centro = -desvio_centro;
-            uint16_t pwm_value = (desvio_centro * 100) / 2048;
-            pwm_set_chan_level(slice, PWM_CHAN_A, pwm_value);
+    // Configura o pino para a função PWM
+    gpio_set_function(21, GPIO_FUNC_PWM);
+
+    // Obtém o slice de PWM associado ao pino
+    uint slice_num = pwm_gpio_to_slice_num(21);
+
+    // Configura o PWM com a frequência desejada
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 4.0f); // Ajusta o divisor de clock conforme necessário
+    // pwm_init(slice_num, &config, true);
+
+    // Define o nível inicial do PWM (0 desliga o buzzer)
+    pwm_set_gpio_level(21, 0);
+
+    while(true) {
+        if(estadoCriticoVolumeAgua || estadoCriticoChuva) {
+            // Atualiza direção da variação de frequência
+            if (subindo) {
+                freq += 50;
+                if (freq >= 2000) subindo = false;
+            } else {
+                freq -= 50;
+                if (freq <= 0) subindo = true;
+            }
+
+            gpio_set_function(21, GPIO_FUNC_PWM);
+            pwm_set_enabled(pwm_gpio_to_slice_num(21), true);
+            uint32_t clock = clock_get_hz(clk_sys);
+            uint32_t divider16 = (clock << 4) / (2000+freq);
+            uint32_t wrap = (divider16 + (1 << 4) - 1) >> 4;
+
+            pwm_set_wrap(slice_num, wrap);
+            pwm_set_chan_level(slice_num, pwm_gpio_to_channel(21), wrap / 8); // 12,5% duty cycle
+            pwm_set_enabled(slice_num, true);
+            vTaskDelay(estadoCriticoChuva?pdMS_TO_TICKS(35):pdMS_TO_TICKS(10)); // Aguarda 50ms para cada tom
+        } else {
+            pwm_set_gpio_level(21, 0);
+            pwm_set_enabled(pwm_gpio_to_slice_num(21), false);
+            gpio_set_function(21, GPIO_FUNC_SIO);
+            gpio_set_dir(21, GPIO_OUT);
+            gpio_put(21, 0);
         }
-        vTaskDelay(pdMS_TO_TICKS(50)); // Atualiza a cada 50ms
     }
 }
-
 
 // Modo BOOTSEL com botão B
 #include "pico/bootrom.h"
@@ -142,8 +223,9 @@ int main() {
     // Criação das tasks
     xTaskCreate(vJoystickTask, "Joystick Task", 256, NULL, 1, NULL);
     xTaskCreate(vDisplayTask, "Display Task", 512, NULL, 1, NULL);
-    xTaskCreate(vLedGreenTask, "LED red Task", 256, NULL, 1, NULL);
-    xTaskCreate(vLedBlueTask, "LED blue Task", 256, NULL, 1, NULL);
+    xTaskCreate(vLedTask, "LED red Task", 256, NULL, 1, NULL);
+    xTaskCreate(vLedMatrixTask, "Matriz de leds Task", 256, NULL, 1, NULL);
+    xTaskCreate(vBuzzerTask, "Buzzer Task", 256, NULL, 1, NULL);
     // Inicia o agendador
     vTaskStartScheduler();
     panic_unsupported();
